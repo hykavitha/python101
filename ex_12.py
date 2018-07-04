5c58a6b55925f87110ffdacabf7a129a3564e258
hykavitha <hykavitha@gmail.com>
2018-07-04 20:35:24 +0530

    sys arguments

---
+++ ex_12.py
@@ -0,0 +1,14 @@
+import sys
+program_name = sys.argv[0]
+arguments = sys.argv[1:]
+count = len(arguments)
+
+
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
