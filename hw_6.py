9a67dfdc034d629621f47e18648b7d73ac96e7f3
hykavitha <hykavitha@gmail.com>
2018-07-05 12:18:22 +0530

    hw6

---
+++ hw_6.py
@@ -0,0 +1,6 @@
+HW.6
+1. WAP to implement all string fnctionalities and comment on each and every function after looking at the result. 
+2. WAP to find largest number of 3 numbers (a, b, c), ask input from users.
+3. HW_5.py to consider input and sysrags
+4. ex_4, ex_15  to consider input and sysrags
+4. Please comment all your code
