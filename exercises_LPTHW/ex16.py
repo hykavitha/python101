c5d175dd1375344ecd77095315bfdc5d2cf8e294
hykavitha <hykavitha@gmail.com>
2018-07-05 23:08:07 +0530

    Create ex16.py

---
+++ exercises_LPTHW/ex16.py
@@ -0,0 +1,12 @@
+# List of numbers
+numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+
+# variable to store the sum
+sum = 0
+
+# iterate over the list
+for val in numbers:
+	sum = sum+val
+
+# Output: The sum is x
+print("The sum is", sum)
