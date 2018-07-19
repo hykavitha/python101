86bd272cb334a0292240ccba6447d870366f3d7c
kavitha yogaraj <hykavitha@gmail.com>
2018-07-20 00:04:30 +0530

    Update ex_27.py

--- exercises_LPTHW/ex_27.py
+++ exercises_LPTHW/ex_27.py
@@ -20,3 +20,16 @@ data['people'].append({
 
 with open('data.txt', 'w') as outfile:  
     json.dump(data, outfile)
+    
+    
+    
+    
+import json
+
+with open('data.txt') as json_file:  
+    data = json.load(json_file)
+    for p in data['people']:
+        print('Name: ' + p['name'])
+        print('Website: ' + p['website'])
+        print('From: ' + p['from'])
+        print('')
