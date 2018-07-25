ed4f2c2af06f9629ea6ec2b18f15216c3fa88ec3
kavitha yogaraj <hykavitha@gmail.com>
2018-07-25 23:06:54 +0530

    Create ex_json_28.py

---
+++ exercises_LPTHW/ex_json_28.py
@@ -0,0 +1,36 @@
+## Read list from the file and 
+## PERFORM some Analysis 
+## each with a unique id, 
+## and each task has a Boolean completed property. Can you determine which users have completed the most tasks?
+
+fo = open('list_json_todo.txt', 'r')
+s = fo.read()
+todos = json.loads(s)
+
+# Map of userId to number of complete TODOs for that user
+todos_by_user = {}
+
+# Increment complete TODOs count for each user.
+for todo in todos:
+    if todo["completed"]:
+        try:
+            # Increment the existing user's count.
+            todos_by_user[todo["userId"]] += 1
+        except KeyError:
+            # This user has not been seen. Set their count to 1.
+            todos_by_user[todo["userId"]] = 1
+
+# Create a sorted list of (userId, num_complete) pairs.
+top_users = sorted(todos_by_user.items(), 
+                   key=lambda x: x[1], reverse=True)
+
+# Get the maximum number of complete TODOs.
+max_complete = top_users[0][1]
+
+# Create a list of all users who have completed
+# the maximum number of TODOs.
+users = []
+for user, num_complete in top_users:
+    if num_complete < max_complete:
+        break
+    users.append(str(user))
