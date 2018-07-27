0ac34decfe490a606308f1cb483f23cdf5a7708d
kavitha yogaraj <hykavitha@gmail.com>
2018-07-28 00:14:16 +0530

    Update ex_42_intersection.py

--- exercises_LPTHW/ex_42_intersection.py
+++ exercises_LPTHW/ex_42_intersection.py
@@ -4,7 +4,13 @@ def intersection(lst1, lst2):
     lst3 = [value for value in lst1 if value in lst2]
     return lst3
  
+
+# of two lists using set() method
+def intersection_set(lst1, lst2):
+    return list(set(lst1) & set(lst2))
+    
 # Driver Code
 lst1 = [4, 9, 1, 17, 11, 26, 28, 54, 69]
 lst2 = [9, 9, 74, 21, 45, 11, 63, 28, 26]
 print(intersection(lst1, lst2))
+print(intersection_set(lst1, lst2))
