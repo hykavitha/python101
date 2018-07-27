d688176d3987413e927e4d91018e36e4461930aa
kavitha yogaraj <hykavitha@gmail.com>
2018-07-28 00:12:12 +0530

    Create ex_41_set_intersection.py

---
+++ exercises_LPTHW/ex_41_set_intersection.py
@@ -0,0 +1,11 @@
+# Python3 program for intersection() function
+ 
+set1 = {2, 4, 5, 6} 
+set2 = {4, 6, 7, 8} 
+set3 = {4,6,8}
+ 
+# union of two sets
+print("set1 intersection set2 : ", set1.intersection(set2))
+ 
+# union of three sets
+print("set1 intersection set2 intersection set3 :", set1.intersection(set2,set3))
