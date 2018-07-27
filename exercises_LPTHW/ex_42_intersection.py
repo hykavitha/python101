5ae0aa2e9d9605891b4598ba9867562b8d8d1b8e
kavitha yogaraj <hykavitha@gmail.com>
2018-07-28 00:13:26 +0530

    Create ex_42_intersection.py

---
+++ exercises_LPTHW/ex_42_intersection.py
@@ -0,0 +1,10 @@
+# Python program to illustrate the intersection
+# of two lists in most simple way
+def intersection(lst1, lst2):
+    lst3 = [value for value in lst1 if value in lst2]
+    return lst3
+ 
+# Driver Code
+lst1 = [4, 9, 1, 17, 11, 26, 28, 54, 69]
+lst2 = [9, 9, 74, 21, 45, 11, 63, 28, 26]
+print(intersection(lst1, lst2))
