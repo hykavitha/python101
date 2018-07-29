b899f8c22a614e121fb5873a97656e337407409d
kavitha yogaraj <hykavitha@gmail.com>
2018-07-29 23:42:12 +0530

    Create ex_43_class.py

---
+++ exercises_LPTHW/ex_43_class.py
@@ -0,0 +1,11 @@
+class Student:  
+   def __init__(self, rollno, name):  
+      self.rollno = rollno  
+      self.name = name  
+   def displayStudent(self):  
+      print "rollno : ", self.rollno,  ", name: ", self.name  
+
+emp1 = Student(121, "Ajeet")  
+emp2 = Student(122, "Sonoo")  
+emp1.displayStudent()  
+emp2.displayStudent() 
