eba48f11863463426fbf04b7dbe2ffc32b9d3c1b
kavitha yogaraj <hykavitha@gmail.com>
2018-08-07 12:06:13 +0530

    Update hw_1.py

--- hw_1.py
+++ hw_1.py
@@ -1,11 +1,14 @@
-import sys
+1. create new ex notepad each time for all the exs, and run that file python filename.py in a command line 
+2. run all exercies from ex_1 to ex_4_1 and add comments to all those, put inpuyt and oputput
 
-pgm_name = sys.argv[0]
-print(pgm_name)
-
-arguments = sys.argv[1:]
-count = len(arguments)
-print(arguments)
-
-
-print("this is Hw_1.py")
+1. WAP to Find the Square Root
+    sqrt = x**(1.0/2)
+2. WAP to find area of the triangle
+    If a, b and c are three sides of a triangle. Then,
+        s = (a+b+c)/2
+        area = √(s(s-a)*(s-b)*(s-c))
+3. WAP to solve quadratic quation
+    The standard form of a quadratic equation is:
+    ax2 + bx + c = 0, where
+    a, b and c are real numbers and
+    a ≠ 0
